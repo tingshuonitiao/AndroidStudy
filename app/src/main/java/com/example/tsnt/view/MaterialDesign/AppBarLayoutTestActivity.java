2d0dba97a4c38483ca8ac2229dd2bf61ceaf6d96
zhangxiaozong <zhangxiaozong@fangdd.com>
2018-01-28 16:18:18 +0800

    learn CoordinatorLayout

---
+++ app/src/main/java/com/example/tsnt/view/MaterialDesign/AppBarLayoutTestActivity.java
@@ -0,0 +1,15 @@
+package com.example.tsnt.view.MaterialDesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tsnt.R;
+
+public class AppBarLayoutTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_bar_layout_test);
+    }
+}
