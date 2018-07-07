7ce4eafc821c69002ddec6ab5ed52106c89d0174
zhangxiaozong <zhangxiaozong@fangdd.com>
2018-07-07 15:07:31 +0800

    add LeakCanary

--- app/src/main/java/com/example/tsnt/TsntApplication.java
+++ app/src/main/java/com/example/tsnt/TsntApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.tsnt.MyEventBusIndex;
+import com.squareup.leakcanary.LeakCanary;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -21,6 +22,8 @@ public class TsntApplication extends Application {
         initEventBus();
         // 初始化ARouter
         initARouter();
+        //初始化LeakCanary
+        initLeakCanary();
     }
 
     private void initEventBus() {
@@ -33,5 +36,9 @@ public class TsntApplication extends Application {
             ARouter.openDebug();           // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
         ARouter.init(this);     // 尽可能早，推荐在Application中初始化
+    }
+
+    private void initLeakCanary() {
+        LeakCanary.install(this);
     }
 }
